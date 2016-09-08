498168222644bf4df59548f1ac3d3a6eaabf87af
Teme <lememta@gmail.com>
2016-09-07 17:22:52 -0700

    deploying results 2

--- deploy_results.sh
+++ deploy_results.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-if [ -n "$GITHUB_API_KEY" ]; then 
-  if [ -n "$TRAVIS_BRANCH" ]; then
-    mkdir -p web
-    git clone https://github.com/jayhorn/jayhorn.git  --branch gh-pages --single-branch web/
-    cd web
-    mkdir -p ./$TRAVIS_BRANCH
-    cp -r ./../jayhorn/build/reports/tests ./$TRAVIS_BRANCH/
-    echo "$PWD"
-    git add ./$TRAVIS_BRANCH
-    git -c user.name='martinschaef' -c user.email='martinschaef@gmail.com' commit -am "AUTO UPDATE from Travis." --no-verify
-    git push -f -q https://martinschaef:$GITHUB_API_KEY@github.com/jayhorn/jayhorn gh-pages 
-    cd .. 
-    echo "DONE updating results"   
-    echo "$PWD"
-  fi
-fi
+mkdir -p benchmark_results
+ls
+git clone https://github.com/jayhorn/jayhorn.git  --branch gh-pages --single-branch benchmark_results
+ls
+cd benchmark_results
+ls
+echo $TRAVIS_BRANCH
+mkdir -p ./$TRAVIS_BRANCH
+cp -r view_results/* ./$TRAVIS_BRANCH/
+echo "$PWD"
+git add ./$TRAVIS_BRANCH
+git -c user.name='lememta' -c user.email='lememta@gmail.com' commit -am "UPDATING Results from Benchmarks" --no-verify
+git push -f -q https://lememta:$GITHUB_API_KEY@github.com/jayhorn/jayhorn gh-pages 
+cd .. 
+echo "DONE updating results"   
+echo "$PWD"
+
+
 
 
