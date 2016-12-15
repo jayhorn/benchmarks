2752ebfcc8dc7969aedd44103cea0af0c76cace0
Rody Kersten <rody.kersten@sv.cmu.edu>
2016-12-15 10:28:31 -0800

    example for resource analysis

---
+++ ImageProcessorMem/src-ra/ConvertImage.java
@@ -0,0 +1,16 @@
+//import java.awt.color.*;
+//import java.awt.image.*;
+//import java.awt.*;
+
+public class ConvertImage
+{    
+    // convert image into grayscale
+    public static BufferedImage otherGray(final BufferedImage image) {
+//        final BufferedImage out = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+//        final Graphics g = out.getGraphics();
+//        g.drawImage(image, 0, 0, null);
+//        return out;
+//    	CostModel.bytes += 1; //image.getWidth() * image.getHeight();
+    	return image;
+    }
+}
---
+++ ImageProcessorMem/src-ra/Convolve.java
@@ -0,0 +1,15 @@
+
+import java.awt.image.Kernel;
+
+
+public class Convolve
+{
+    public static final Kernel Gausian5x5 = new Kernel(5, 5, new float[] { 0.003021148f, 0.012084592f, 0.021148037f, 0.012084592f, 0.003021148f, 0.012084592f, 0.06042296f, 0.09969789f, 0.06042296f, 0.012084592f, 0.021148037f, 0.09969789f, 0.16616315f, 0.09969789f, 0.021148037f, 0.012084592f, 0.06042296f, 0.09969789f, 0.06042296f, 0.012084592f, 0.003021148f, 0.012084592f, 0.021148037f, 0.012084592f, 0.003021148f });
+;    
+    public static BufferedImage convolve(final BufferedImage image, final Kernel kernel) {
+    	// removed library call
+//        return new ConvolveOp(kernel, 1, null).filter(image, null);
+//    	CostModel.bytes += 1; //image.getWidth() * image.getHeight();
+    	return image;
+    }
+}
