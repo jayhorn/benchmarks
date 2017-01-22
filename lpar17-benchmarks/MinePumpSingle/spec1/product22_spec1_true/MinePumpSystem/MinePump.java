f5c7b281a79fc79f1ccb932fea566c9aca57f088
Teme <lememta@gmail.com>
2017-01-22 08:57:01 -0800

    lpar bench

---
+++ lpar17-benchmarks/MinePumpSingle/spec1/product22_spec1_true/MinePumpSystem/MinePump.java
@@ -0,0 +1,117 @@
+package MinePumpSystem; 
+
+import MinePumpSystem.Environment; 
+
+public   class  MinePump {
+	
+
+	boolean pumpRunning = false;
+
+	
+
+	boolean systemActive = true;
+
+	
+
+	Environment env;
+
+	
+
+	public MinePump(Environment env) {
+		super();
+		this.env = env;
+	}
+
+	
+
+	public void timeShift() {
+		if (pumpRunning)
+			env.lowerWaterLevel();
+		if (systemActive)
+			processEnvironment();
+	}
+
+	
+	private void  processEnvironment__wrappee__base  () {
+		
+	}
+
+	
+
+	private void  processEnvironment__wrappee__lowWaterSensor  () {
+		if (pumpRunning && isLowWaterLevel()) {
+			deactivatePump();
+		} else {
+			processEnvironment__wrappee__base();
+		}
+	}
+
+	
+	public void processEnvironment() {
+		if (pumpRunning && isMethaneAlarm()) {
+			deactivatePump();
+		} else {
+			processEnvironment__wrappee__lowWaterSensor();
+		}
+	}
+
+	
+
+	void activatePump() {
+		pumpRunning = true;
+	}
+
+	
+
+	public boolean isPumpRunning() {
+		return pumpRunning;
+	}
+
+
+	void deactivatePump() {
+		pumpRunning = false;
+	}
+
+	
+	
+	boolean isMethaneAlarm() {
+		return env.isMethaneLevelCritical();
+	}
+
+	
+
+	@Override
+	public String toString() {
+		return "Pump(System:" + (systemActive?"On":"Off") + ",Pump:" + (pumpRunning?"On":"Off") +") " + env.toString(); 
+	}
+
+	
+	
+	public Environment getEnv() {
+		return env;
+	}
+
+	
+	
+	boolean isLowWaterLevel() {
+		return !env.isLowWaterSensorDry();
+	}
+
+	
+	public void startSystem() {
+		assert !pumpRunning;
+		systemActive = true;
+	}
+
+
+	public void stopSystem() {
+		// feature not present
+	}
+
+
+	public boolean isSystemActive() {
+		return systemActive;
+	}
+
+
+}
